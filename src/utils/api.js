00c5272805c2315b01d33366d7c96f9364fbe3b3
SHIVAM KUMAR SHAW <108209696+shivamshaw07@users.noreply.github.com>
2024-05-24 16:12:50 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const options = {
     params: { hl: "en", gl: "US" },
     headers: {
         "X-RapidAPI-Key":
-        '5646029cc3msh8a3c3c2eac6330fp1f252ejsnd9d52bb37b18',
+        '0d94ba254bmsh068191b8c3ffb02p113c81jsn35fa5ed1c141',
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
